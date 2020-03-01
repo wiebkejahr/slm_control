bug fixes for Zernike scaling, code cleanup. Gratings still scale wrongly

Pattern_Calculator.py
@@ -21,11 +21,13 @@ def load_image(img_path):
     image = np.array(im)
     return image
 
+
 def save_image(img, img_path, img_name):
     """ Saves the provided matrix img as an image under the path img_path and
         the name img_name. """
     image = Image.fromarray((img*255).astype(np.uint8))
     image.save(img_path+img_name)   
+
         
 def add_images(images):
     """ Takes and array of input images, and the image size. Then adds all
@@ -35,11 +37,13 @@ def add_images(images):
         image = image + img
     return image
 
+
 def stitch_images(img_l, img_r):
     """ Stitches the provided left and right images into one image of twice the
         width."""
     stitched = np.hstack((img_l, img_r))
     return stitched
+
 
 def phase_wrap(image, phase):
     """ Phase wraps the image into the available contrast by performing a
@@ -47,17 +51,19 @@ def phase_wrap(image, phase):
     pw = np.mod(image, phase)
     return pw
 
+
 def cart2polar(x, y):
     """ Returns normalized polar coordinates for cartesian inputs x and y. """
     z = x + 1j * y
     z_norm = np.mean([(np.max(x) - np.min(x)), (np.max(y) - np.min(y))])
     return(np.abs(z) / z_norm, np.angle(z))
-    #return (np.abs(z)/np.max(np.abs(z)), np.angle(z))
+
 
 def polar2cart(r, theta):
     """ Returns cartesian coordinates for r and theta polar coordinates. """
     z = r * np.exp( 1j * theta)
     return (np.real(z), np.imag(z))
+
 
 def normalize_img(img):
     """ Normalizes the image data  to [0,1]."""
@@ -68,22 +74,22 @@ def normalize_img(img):
 
     return img_norm
 
+
 def normalize_radius(obj_ba, mag, slm_px, size_slm):
     """ Normalizes the radius to the size of the SLM pixels and the radius of
         the laser beam. """
-    
-    #TODO radius_slm = obj_ba / 2 / mag / slm_px / np.mean(size_slm * 2)
     radius_slm = obj_ba / mag / slm_px / np.mean(size_slm * 2)
     print("radius_slm ", radius_slm, "mag ", mag)
-    #radius_slm = 1
     
     return radius_slm
+
 
 def bfp_radius(M, NA, f_TL):
     """ Takes magnification M and NA of the objective, as well as focal length
         of the tube lens, to calculate the radius of the objective's back focal
         plane. """
     return NA * f_TL / M
+
 
 def crop(full, size, offset = [0,0]):
     """ Crops the full data to half the size, using the provided offset. 
@@ -94,6 +100,7 @@ def crop(full, size, offset = [0,0]):
     maxy = int(size[1]*3/2 - offset[0])    
     cropped = full[minx:maxx, miny:maxy]
     return cropped
+
 
 def create_coords(size, off = [0,0]):
     """ Returns the cartesian coordinates of each pixel in a matrix from the 
@@ -119,12 +126,9 @@ def zernike_coeff(rho, order):
              mfac((nn + mm)/2 - kk) * \
              mfac((nn - mm)/2 - kk))
         r = (np.power(rho, nn - 2 * kk))
-        #print("c ", c)
-        coeff = coeff + c * r
-    
-    #print(np.shape(coeff))
-    #print(coeff, np.power(coeff, 2))
+        coeff = coeff + c * r   
     return coeff
+
 
 def create_zernike(size, order, amp = 1, radscale=1):
     """ Calculates the Zernike polynomial of a given order, for the given 
@@ -151,10 +155,7 @@ def create_rect(size, a, b, rot, amp = 1, radscale = 1):
     xcoord, ycoord = create_coords(size)
     rho, phi = cart2polar(xcoord, ycoord)
     rho = rho * 4 / radscale
-        
-    #rect1 = (rho <= a / 2 / np.cos(phi)) * (np.tan(phi) <= b / a )
-    #rect2 = (rho <= b / 2 / np.sin(phi)) * (np.tan(phi) > b / a )
-    #rect = rect1 + rect2
+
     rect = rho <= np.minimum(a / np.abs(np.cos(phi - (rot + 180) / 180 * np.pi)), 
                              b / np.abs(np.sin(phi - (rot + 180) / 180 * np.pi)))
     return rect * amp

Patterns_Zernike.py
@@ -17,17 +17,14 @@ class Sub_Pattern_Zernike(Sub_Pattern):
         weights, usually of the two orthogonal Zernike modes (primary and 
         secondary for spherical). Recalculates the aberration and calls an 
         update to the Aberr_Pattern instance to recalculate aberrations. """
-    def __init__(self, params, order, name, parent = None):
+    def __init__(self, params, name, parent = None):
         super(Sub_Pattern, self).__init__(parent)
         self.size = np.asarray(params.general["size_slm"]) * 2
-        self.order = order
-#        self.radnorm = pcalc.normalize_radius(params.general["laser_radius"], 
-#                                      params.general["slm_px"],
-#                                      params.general["size_slm"])
+
         self.data = np.zeros(self.size)
         self.coeff = [0,0]
-        self.set_name(name)
         
+        self.set_name(name)        
         
     def set_name(self, name):
         self.name = name
@@ -39,6 +36,7 @@ class Sub_Pattern_Zernike(Sub_Pattern):
         # somewhat ugly solution for using identical correction on both sides of the SLM
         # whenever one side is changed, just writes the values into the gui controls
         # of the other side, then updates the images accordingly
+        # seems to be rather slow to go through the if statements?
         if self.daddy.daddy.blockupdating == False:
             if self.daddy.daddy.daddy.sngl_corr_state.checkState():
                 if self.daddy.daddy.get_name() == "img_l":
@@ -74,18 +72,6 @@ class Sub_Pattern_Zernike(Sub_Pattern):
                     elif self.get_name() == "trefoil":
                         otherside.trefoil.xgui.setValue(thisside.trefoil.xgui.value())
                         otherside.trefoil.ygui.setValue(thisside.trefoil.ygui.value())
-    
-            #data_a = pcalc.create_zernike(self.size, self.order[0], self.daddy.daddy.daddy.slm_radius)
-            #data_b = pcalc.create_zernike(self.size, self.order[1], self.daddy.daddy.daddy.slm_radius)
-            #self.data = self.xgui.value() * data_a + self.ygui.value() * data_b
-            
-            #self.data = self.xgui.value() * zernike[0] + self.ygui.value() * zernike[1]
-            
-            #TODO: copy over create_gui and double_spin from init of subpattern class
-            #change such that it accepts a string?
-            # and then pass the name of the changed aberration into it
-            # alternatively: just recalc all? they need to be added anyway
-            # does multiplication make things so much slower?
             
             self.coeff = [self.xgui.value(), self.ygui.value()]
             
@@ -123,30 +109,25 @@ class Aberr_Pattern(QtWidgets.QWidget):
         defset = [[3, 0.1, -10, 10], [3, 0.1, -10, 10]]         
         
         gui = QtWidgets.QGridLayout()
-        self.astig = Sub_Pattern_Zernike(p, [[2,2], [2,-2]], "astig")
+        self.astig = Sub_Pattern_Zernike(p, "astig")
         gui.addLayout(self.astig.create_gui(p_abberation["astig"], defset, layout = 'h'), 0,0,1,2)
         self.astig.call_daddy(self)
-        #self.astig.set_name("astig")
         self.astig.compute_pattern(update = False)
         
-        self.coma = Sub_Pattern_Zernike(p, [[3,1], [3,-1]], "coma")
+        self.coma = Sub_Pattern_Zernike(p, "coma")
         gui.addLayout(self.coma.create_gui(p_abberation["coma"], defset, layout = 'h'), 1,0,1,2)  
         self.coma.call_daddy(self)
-        #self.coma.set_name("coma")
         self.coma.compute_pattern(update = False)
         
-        self.sphere = Sub_Pattern_Zernike(p, [[4,0], [6,0]], "sphere")
+        self.sphere = Sub_Pattern_Zernike(p, "sphere")
         gui.addLayout(self.sphere.create_gui(p_abberation["sphere"], defset, layout = 'h'), 2,0,1,2)
         self.sphere.call_daddy(self)
-        #self.sphere.set_name("sphere")
         self.sphere.compute_pattern(update = False)
         
-        self.trefoil = Sub_Pattern_Zernike(p, [[3,3], [3,-3]], "trefoil")
+        self.trefoil = Sub_Pattern_Zernike(p, "trefoil")
         gui.addLayout(self.trefoil.create_gui(p_abberation["trefoil"], defset, layout = 'h'), 3,0,1,2)
         self.trefoil.call_daddy(self)
-        #self.trefoil.set_name("trefoil")
         self.trefoil.compute_pattern(update = False)
-        
         
         gui.setContentsMargins(0,0,0,0)
         return gui
@@ -162,7 +143,6 @@ class Aberr_Pattern(QtWidgets.QWidget):
                                       z["sphere1"]  * self.sphere.coeff[0],
                                       z["sphere2"]  * self.sphere.coeff[1]
                                      ])
-        #self.data = pcalc.add_images([self.astig.data, self.coma.data, self.sphere.data, self.trefoil.data])
         if update:
             self.daddy.update()
         return self.data