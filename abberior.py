98cee02f65234c0a184504f198a69e8db8e4d481
hmcgovern <56073830+hmcgovern@users.noreply.github.com>
2020-03-03 15:08:16 +0100

    Squashed 'autoalign/' changes from b6df462..574122d
    574122d exception handling for specpy package
    a77dad2 small changes
    7b927b8 troubleshooting xsection
    273f7a5 Merge commit 'c7003f4a875f1bb1469c0d0c00d47b468aedbe49' as 'autoalign'
    5147776 removing autoalgin
    83a01af final tweaks to pattern generation
    f122a0b take 2
    98f1f5b improving if/else statements
    8f17780 gross hack
    1a8be70 gross hack
    1e6f606 removing raise statement
    a24b032 Merge branch 'semistable_fortesting' of https://git.ist.ac.at/wjahr/slm_control into semistable_fortesting
    d512e6d try/except for specpy
    5dcb8c1 Merge branch 'semistable_fortesting' of git.ist.ac.at:wjahr/slm_control into semistable_fortesting
    3e1494a image w latest pattern
    375b51a fixing imports and fn call
    6674f4d updating READMEs
    d13c0e1 Merge commit '87b57741e5b05d337024a2e9426121bbbcc957e6' as 'autoalign'
    87b5774 Squashed 'autoalign/' content from commit f8049c5
    
    git-subtree-dir: autoalign
    git-subtree-split: 574122d542e58d50f918c3b23dd0a72a8646335b

--- abberior.py
+++ abberior.py
@@ -18,8 +18,8 @@ import matplotlib.pyplot as plt
 try:
     import specpy as sp
 except:
-    raise("Specpy not installed!")
-    exit(0)
+    print("Specpy not installed!")
+    pass
 
 from autoalign.utils.integration import integrate
 import autoalign.utils.helpers as helpers
@@ -30,6 +30,7 @@ def test(model, input_image, model_store_path):
     # load the model weights from training
     checkpoint = torch.load(model_store_path)
     model.load_state_dict(checkpoint['model_state_dict'])
+    
 
     ideal_coeffs = np.asarray([0.0]*12)
 
@@ -40,7 +41,10 @@ def test(model, input_image, model_store_path):
     
     with torch.no_grad():
         # adds 3rd color channel dim and batch dim 
-        image = torch.from_numpy(input_image).unsqueeze(0).unsqueeze(0)
+        # NOTE: THIS IS ONLY FOR 1D
+        # image = torch.from_numpy(input_image).unsqueeze(0).unsqueeze(0)
+        # NOTE: THIS IS ONLY FOR 3D
+        image = torch.from_numpy(input_image).unsqueeze(0)
        
         # avg = []
         # i = 0
@@ -48,19 +52,19 @@ def test(model, input_image, model_store_path):
             # pass it through the trained model to get the predicted coeffs
         outputs = model(image)
     coeffs = outputs.numpy().squeeze()
-        # avg.append(coeffs)
-        # i += 1
-    # avg = np.stack(avg)
-    # avg = np.average(avg, axis=0)
-    # coeffs = avg
-    # corrected = normalize_img(input_image) + normalize_img(get_psf(-coeffs))
-    # plt.figure()
-    # plt.imshow(corrected)
-    # plt.show()
+    #     # avg.append(coeffs)
+    #     # i += 1
+    # # avg = np.stack(avg)
+    # # avg = np.average(avg, axis=0)
+    # # coeffs = avg
+    # # corrected = normalize_img(input_image) + normalize_img(get_psf(-coeffs))
+    # # plt.figure()
+    # # plt.imshow(corrected)
+    # # plt.show()
 
    
-    # print("\n\n correlation coeff is: {} \n\n".format(np.corrcoef(donut.flat, corrected.flat)[0][1]))
-    # return coeffs, np.corrcoef(donut.flat, corrected.flat)[0][1], corrected
+    # # print("\n\n correlation coeff is: {} \n\n".format(np.corrcoef(donut.flat, corrected.flat)[0][1]))
+    # # return coeffs, np.corrcoef(donut.flat, corrected.flat)[0][1], corrected
     return coeffs
     
 
@@ -80,7 +84,7 @@ def correct(model_store_path):
     msr = im.active_measurement()
     image = msr.stack('ExpControl Ch1 {1}').data() # converts it to a numpy array
     
-    image = preprocess(image)
+    image = helpers.preprocess(image)
     # # a little preprocessing
     # image = normalize_img(np.squeeze(image)) # normalized (200,200) array
     # image = crop_image(image, tol=0.1) # get rid of dark line on edge
@@ -115,6 +119,7 @@ def correct(model_store_path):
 
 def abberior_multi(model_store_path):
     # creates an instance of CNN
+    
     model = my_models.MultiNet()
 
     # acquire the image from Imspector    
@@ -127,23 +132,35 @@ def abberior_multi(model_store_path):
     # get active measurement 
     msr = im.active_measurement()
     image_xy = msr.stack('ExpControl Ch1 {1}').data() # converts it to a numpy array
-    image_xy = preprocess(image_xy)
+    image_xy = helpers.preprocess(image_xy)
+    plt.figure()
+    plt.imshow(image_xy)
+    plt.show()
 
     ##### NOTE: fill this is in lab #######
-    # image_xz = msr.stack('')
-    # image_xz = preprocess(image_xz)
+    image_xz = msr.stack('ExpControl Ch1 {13}').data()
+    image_xz = helpers.preprocess(image_xz)
+    plt.figure()
+    plt.imshow(image_xz, aspect="equal")
+    plt.show()
     
-    # image_yz = msr.stack('')
-    # image_yz = preprocess(image_yz)
+    image_yz = msr.stack('ExpControl Ch1 {15}').data()
+    image_yz = helpers.preprocess(image_yz)
+    plt.figure()
+    plt.imshow(image_yz, aspect="equal")
+    plt.show()
     # ##################
     
+    # exit()
     
 
-    # image = np.stack((image_xy, image_xz, image_yz), axis=0)
+    image = np.stack((image_xy, image_xz, image_yz), axis=0)
+
     
-    # coeffs, _, image = test(model, image, model_store_path)
+    # # coeffs, _, image = test(model, image, model_store_path)
     coeffs = test(model, image, model_store_path)
 
+    print(coeffs)
     # a dictionary of correction terms to be passed to SLM control
     corrections = {
             "sphere": [
