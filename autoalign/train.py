86ef82a094e2793cffadcc811ce5e537febf6182
hmcgovern <56073830+hmcgovern@users.noreply.github.com>
2020-08-13 16:41:35 -0400

    removing the 224 x 224

--- autoalign/train.py
+++ autoalign/train.py
@@ -171,6 +171,7 @@ def main(args):
     # tsfms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), my_classes.Normalize(mean=mean, std=std), my_classes.Noise(bgnoise=2, poiss=350)])
     
     tsfms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])#, my_classes.Noise(bgnoise=2, poiss=350)])
+    tsfms = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])#, my_classes.Noise(bgnoise=2, poiss=350)])
     
     train_dataset = my_classes.PSFDataset(hdf5_path=data_path, mode='train', transform=tsfms)
     val_dataset = my_classes.PSFDataset(hdf5_path=data_path, mode='val', transform=tsfms)
