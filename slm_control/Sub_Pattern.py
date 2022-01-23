6e9b1573babbb58985dd5430d36b457842a26c69
Wiebke Jahr <wiebke@Wiebkes-MacBook-Pro.local>
2022-01-23 21:17:58 +0100

    cleanup. ToDo: Load Vortex from file

--- slm_control/Sub_Pattern.py
+++ slm_control/Sub_Pattern.py
@@ -18,10 +18,7 @@ class Sub_Pattern(QtWidgets.QWidget):
         any of the GUI elements. """
     def __init__(self, params, size, parent = None):
         super(Sub_Pattern, self).__init__(parent)
-        #super().__init__()
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        #self.size = np.asarray(params.general["size_slm"]) * 2
-        #TODO: Double check! here it's size * 2 ... for the grid and offset only size
         self.size = size * 2
         self.data = np.zeros(self.size)
     
@@ -63,20 +60,14 @@ class Sub_Pattern(QtWidgets.QWidget):
         item.valueChanged.connect(lambda: self.compute_pattern())
         main_layout.addWidget(item)
         return item
-    
-    
+        
 
 class Off_Pattern(Sub_Pattern):
     """ Subpattern containg the GUI for setting the offsets. Calls the crop 
         function of the calling instance whenever the offset values are changed. """
     def __init__(self, params, size, parent = None):
-        super(Sub_Pattern, self).__init__(parent)        
-        #I don't get why, but it seems initializing the variables in the subclass
-        # seems necessary
-        # if only initialized in the super class, they are created as function,
-        # not as variable        
+        super(Sub_Pattern, self).__init__(parent)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        #self.size = params.general["size_slm"]
         self.size = size
         self.value = [0,0]
 
@@ -84,8 +75,6 @@ class Off_Pattern(Sub_Pattern):
         if self.daddy.blockupdating == False:
             self.value = np.asarray([self.xgui.value(), self.ygui.value()])
             self.daddy.offset = self.value
-            #TODO: put a check for double pass status here & call flatfield if
-            # needed to restitch the two images
             if self.daddy.daddy.dbl_pass_state.checkState() and self.daddy.daddy.flt_fld_state.checkState():
                 self.daddy.daddy.load_flat_field(self.daddy.daddy.p.left["cal1"], self.daddy.daddy.p.right["cal1"])
             self.daddy.crop()
@@ -97,13 +86,8 @@ class Sub_Pattern_Grid(Sub_Pattern):
         the grating, and calls an update to the Half Pattern to recalculate the
         whole image data. """
     def __init__(self, params, size, parent = None):
-        super(Sub_Pattern, self).__init__(parent)        
-        #I don't get why, but it seems initializing the variables in the subclass
-        # seems necessary
-        # if only initialized in the super class, they are created as function
-        # not as variable        
+        super(Sub_Pattern, self).__init__(parent)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        #self.size = np.asarray(params.general["size_slm"])
         self.size = size
         self.slm_px = params.general["slm_px"]
         self.data = np.zeros(self.size)
@@ -130,7 +114,6 @@ class Sub_Pattern_Vortex(Sub_Pattern):
     def __init__(self, params, size, parent = None):
         super(Sub_Pattern, self).__init__(parent) 
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        #self.size = np.asarray(params.general["size_slm"]) * 2
         self.size = size * 2
         self.data = np.zeros(self.size)
         self.path = params.general["path"]
@@ -150,10 +133,6 @@ class Sub_Pattern_Vortex(Sub_Pattern):
         self.rotgui = self.double_spin(defval[3], [2, 10, 0, 360], gui)
         self.stepgui = self.double_spin(defval[4], [0, 1, 0, 360], gui)
         self.modegui.activated.connect(lambda: self.compute_pattern())
-        
-        #TODO: added temporarily, needs to be removed later again
-        #self.tempscalegui = self.double_spin(1, [2, 0.1, -10, 10], gui)
-        
         gui.setContentsMargins(0,0,0,0)
         
         return gui
@@ -182,6 +161,7 @@ class Sub_Pattern_Vortex(Sub_Pattern):
                 self.data = pcalc.compute_vortex('Gauss', self.size, rot, rad, 
                                                  steps, phase, slm_scale)
                 print("From File")
+                #TODO: implement!
             
             if update:
                 self.daddy.update()
@@ -250,7 +230,6 @@ class Sub_Pattern_Defoc(Sub_Pattern):
         Half Pattern to recalculate the whole image data. """
     def __init__(self, params, size, parent = None):
         super(Sub_Pattern, self).__init__(parent)
-        #self.size = np.asarray(params.general["size_slm"]) * 2
         self.size = size * 2
         self.data = np.zeros(self.size)
         
